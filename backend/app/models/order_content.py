40cb90fb7537487366e32366f4ab85e8bea77f59
BrBriz <bletbriz@gmail.com>
2026-08-30 00:24:21 +0300

    Пропущений файл

--- backend/app/models/order_content.py
+++ backend/app/models/order_content.py
@@ -1,12 +1,11 @@
-from __future__ import annotations
 from typing import TYPE_CHECKING # ignoring this in runtime
 
 from sqlalchemy import (
-    Integer, Text, Boolean, ForeignKey, Column
+    Integer, ForeignKey
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.database import Base
+from app.db.database import Base
 
 if TYPE_CHECKING:
     from app.models import Clothing, Order
