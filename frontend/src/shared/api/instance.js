0ec21349b1ff5d81640df2b3e2be2ee763fc6104
MHanushchak <misha.ganushchak@gmail.com>
2026-09-15 15:43:12 +0300

    commit 2

--- frontend/src/shared/api/instance.js
+++ frontend/src/shared/api/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = process.env.REACT_APP_API_URL;
+export const API_BASE_URL = process.env.API_URL;
 
 export const api = axios.create({
   withCredentials: true,
